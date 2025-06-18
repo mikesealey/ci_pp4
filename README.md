474db398669422b7383f1cf638475cfa8d23d677
mikesealey <mikesealey@hotmail.com>
2025-06-18 20:59:11 +0100

    favicon

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 Background Image: https://www.pexels.com/photo/focused-artisan-making-hole-on-plank-with-hammer-and-chisel-5974283/
 
+Icon Image: https://iconduck.com/icons/157332/hammer
+
--- the_woodshed/settings.py
+++ the_woodshed/settings.py
@@ -147,6 +147,8 @@ SITE_ID =1
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
