130b35c5a355c85c7a74546ac8ec15ac8d292661
mikesealey <mikesealey@hotmail.com>
2025-06-18 22:39:32 +0100

    begins work on product catalogue

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    material = models.CharField(max_length=100)
+    finish = models.CharField(max_length=100)
+    images = models.JSONField()  # JSON will hold an arrya of image-URLS
+    qty_in_stock = models.PositiveIntegerField()
