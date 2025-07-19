84be71c6158f9d6417913db960656f6c3091d2ee
mikesealey <mikesealey@hotmail.com>
2025-07-19 16:59:14 +0100

    builds past orders

--- orders/views.py
+++ orders/views.py
@@ -6,4 +6,6 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def my_past_orders(request):
     orders = Order.objects.filter(user=request.user).order_by('-id')
+    for order in orders:
+        order.total = sum(item.subtotal() for item in order.items.all())
     return render(request, "my_past_orders.html", {"orders": orders})
