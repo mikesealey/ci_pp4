05cac520b547400482cb5a090d20b9fb5701c0a3
mikesealey <mikesealey@hotmail.com>
2025-07-26 16:25:27 +0100

    adds whitenoise to middleare

--- the_woodshed/settings.py
+++ the_woodshed/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'the_woodshed.urls'
