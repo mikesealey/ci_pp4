2a2732a59b8f24e4311aeb8a4ff4f8712c2598c8
mikesealey <mikesealey@hotmail.com>
2025-07-30 20:30:38 +0100

    reshapes image handling in db

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductImage
 
 # Register your models here.
-admin.site.register(Product)
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
