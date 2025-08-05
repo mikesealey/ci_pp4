50d786caaf1cc0625077f0e52ab08a97341b62b0
mikesealey <mikesealey@hotmail.com>
2025-08-05 19:08:23 +0100

    writes equivilent basket tests for wishlist

--- wishlist/tests.py
+++ wishlist/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from catalogue.models import Product
+from django.urls import reverse
+from wishlist.models import Wishlist, WishlistItem
 
 # Create your tests here.
+class WishlistTests(TestCase):
+    # In the current version of The Woodshed - adding products to wishlist or basket can only be done as a logged in user
+    def setUp(self):
+        # Creates a user and product
+        self.client = Client()
+        self.user = User.objects.create_user(username="testuser", password="pass")
+        self.product = Product.objects.create(
+            name="Test Product",
+            price=50,
+            qty_in_stock=5,
+            category="Furniture"
+        )
+
+    def login(self):
+        # Logs user in
+        self.client.login(username="testuser", password="pass")
+
+    def test_add_to_wishlist_creates_wishlist_and_item(self):
+        # Add one item - item quantity should be 1
+        self.login()
+        response = self.client.get(reverse("add_to_wishlist", args=[self.product.id]))
+        self.assertRedirects(response, reverse("products_list"))
+        wishlist = Wishlist.objects.get(user=self.user)
+        item = wishlist.items.get(product=self.product)
+        self.assertEqual(item.qty, 1)    
+
+    def test_add_to_wishlist_increments_qty(self):
+        # Add two items - item quantity should be 2
+        self.login()
+        self.client.get(reverse("add_to_wishlist", args=[self.product.id]))
+        self.client.get(reverse("add_to_wishlist", args=[self.product.id]))
+        item = Wishlist.objects.get(user=self.user).items.get(product=self.product)
+        self.assertEqual(item.qty, 2)
+
+    def test_remove_from_wishlist(self):
+        # Create a wishlist with an item, remove the item, check if it exists
+        self.login()
+        wishlist = Wishlist.objects.create(user=self.user)
+        WishlistItem.objects.create(wishlist=wishlist, product=self.product, qty=1)
+        response = self.client.get(reverse("remove_from_wishlist", args=[self.product.id]))
+        self.assertRedirects(response, reverse("my_wishlist"))
+        self.assertFalse(WishlistItem.objects.filter(wishlist=wishlist).exists())
