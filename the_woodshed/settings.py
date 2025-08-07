514d83befb0dbc0f6276d1cf770a903cfb614edf
mikesealey <mikesealey@hotmail.com>
2025-08-07 20:05:16 +0100

    resets basket interval after testing

--- the_woodshed/settings.py
+++ the_woodshed/settings.py
@@ -185,4 +185,4 @@ DOMAIN_URL = os.environ.get("DOMAIN_URL")
 # STOCK RESERVATIONS
 # how long reservations last (hours)
 # Items added to a basket will immediately impart qty_in_stock, abandonned baskets will return the stock levels after this timeout
-BASKET_RESERVATION_HOURS = 0.01
+BASKET_RESERVATION_HOURS = 3
