dd25dd5f6f42a3dc2c8ac2d7cc5ce2f47ad7228a
mikesealey <mikesealey@hotmail.com>
2025-08-08 20:31:34 +0100

    separates out JS from product details

---
+++ static/scripts/gallery.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    // Django returns 'Python JSON' which uses single-quotes which isn't compatable with JS-JSON
+    // Processing Python JSON into legit-JSON
+
+    const imageArray = $("#image-data").text()
+    const processedImageArray = imageArray.replaceAll("'", '"')
+    const images = JSON.parse($('#image-data').text());
+
+    let index = -1;
+
+    function updateGallery() {
+    const leftIndex = (index + images.length) % images.length
+    const centreIndex = (index + 1) % images.length
+    const rightIndex = (index + 2) % images.length
+
+    $("#left-image").attr("src", images[leftIndex].url)
+    $("#centre-image").attr("src", images[centreIndex].url)
+    $("#right-image").attr("src", images[rightIndex].url)
+    $("#img-counter").text(`${index+1}/${images.length}`)
+
+    }
+
+    $("#left-image").on("click", () => {
+    index = (index - 1 + images.length) % images.length;
+    updateGallery()
+
+    });
+
+    $("#centre-image").on("click", () => {
+    index = (index - 1 + images.length) % images.length;
+    updateGallery()
+    });
+
+    $("#right-image").on("click", () => {
+    index = (index + 1) % images.length
+    updateGallery()
+    });
+
+    updateGallery()
+})
+
+
