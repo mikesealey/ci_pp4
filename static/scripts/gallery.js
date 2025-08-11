eb1698a0ce09252b3f7e0b8281c9ee7910480173
mikesealey <mikesealey@hotmail.com>
2025-08-11 08:26:32 +0100

    prevents empty JSON error

--- static/scripts/gallery.js
+++ static/scripts/gallery.js
@@ -2,35 +2,39 @@ $(document).ready(function(){
     // Django returns 'Python JSON' which uses single-quotes which isn't compatable with JS-JSON
     // Processing Python JSON into legit-JSON
     const imageArray = $("#image-data").text()
+    if (!imageArray.length){
+        // Prevents script from running when not viewing the image gallery
+        return
+    }
     const processedImageArray = imageArray.replaceAll("'", '"')
     const images = JSON.parse(processedImageArray);
 
     let index = 0;
 
     function updateGallery() {
-    const leftIndex = ((index - 1) + images.length) % images.length
-    const centreIndex = (index) % images.length
-    const rightIndex = (index + 1) % images.length
+        const leftIndex = ((index - 1) + images.length) % images.length
+        const centreIndex = (index) % images.length
+        const rightIndex = (index + 1) % images.length
 
-    $("#left-image").attr("src", images[leftIndex].url)
-    $("#centre-image").attr("src", images[centreIndex].url)
-    $("#right-image").attr("src", images[rightIndex].url)
-    $("#img-counter").text(`${index+1}/${images.length}`)
-    }
+        $("#left-image").attr("src", images[leftIndex].url)
+        $("#centre-image").attr("src", images[centreIndex].url)
+        $("#right-image").attr("src", images[rightIndex].url)
+        $("#img-counter").text(`${index+1}/${images.length}`)
+        }
 
-    $("#left-image").on("click", () => {
-    index = (index - 1 + images.length) % images.length;
-    updateGallery()
-    });
+        $("#left-image").on("click", () => {
+        index = (index - 1 + images.length) % images.length;
+        updateGallery()
+        });
 
-    $("#centre-image").on("click", () => {
-    index = (index - 1 + images.length) % images.length;
-    updateGallery()
-    });
+        $("#centre-image").on("click", () => {
+        index = (index - 1 + images.length) % images.length;
+        updateGallery()
+        });
 
-    $("#right-image").on("click", () => {
-    index = (index + 1) % images.length
-    updateGallery()
+        $("#right-image").on("click", () => {
+        index = (index + 1) % images.length
+        updateGallery()
     });
 
     updateGallery()
